10320a1d79102bf38bb2afcba366eb1e96288c45
farhadulh98 <66354602+farhadulh98@users.noreply.github.com>
2020-06-06 00:20:17 +0500

    Changes

--- ak/src/App.js
+++ ak/src/App.js
@@ -1,40 +1,10 @@
 import React, { Component } from 'react'
-import ATable from './table'
-import Form from './Form'
 class App extends Component {
-  state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]
-  }
-  removeCharacter = index =>{
-    const chars  = this.state.characters
-    this.setState({
-      characters:chars.filter((chars,i) =>{
-        return i !== index
-      }),
-    })
-  }
+  
+    
+
   render() {
-    const { characters } = this.state
-    // const { charss } = this.state
-    console.log("ayy")  
-    console.log(characters)
+
 
     return (
       <div className='col-xs-12'>      
